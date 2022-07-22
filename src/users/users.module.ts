bca242f8418313e81544dd9573d77194ea583f84
Aleksandr Kurakin <a.kurakin@cian.ru>
2022-07-22 11:12:14 +0300

    begin add roles auth decorator

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -12,9 +12,9 @@ import { AuthModule } from "../auth/auth.module";
     controllers: [UsersController],
     providers: [UsersService],
     imports: [
+        forwardRef(() => AuthModule),
         SequelizeModule.forFeature([User, Role, UserRoles]),
-        RolesModule,
-        forwardRef(() => AuthModule)
+        RolesModule
     ],
     exports: [
         UsersService
